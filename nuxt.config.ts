bedc618468dddface0aa45e67afe2e7a77dd2d34
Zein Ersyad <zeinersyad@gmail.com>
2023-07-25 13:16:14 +0700

    fix(BigInt): move to prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
+    // @ts-ignore
     app: {
         head: {
             title: 'Transaction Tracker',
--- server/models/prisma.ts
+++ server/models/prisma.ts
@@ -7,3 +7,17 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+/// https://github.com/prisma/studio/issues/614#issuecomment-1374116622
+declare global {
+    interface BigInt {
+        toJSON(): string;
+    }
+}
+
+
+// @ts-ignore
+BigInt.prototype.toJSON = function () {
+    const int = Number.parseInt(this.toString());
+    return int ?? this.toString();
+};
