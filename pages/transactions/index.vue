968fb90c4e4e1e05e83be228cc3aea8e75c151b7
Zein Ersyad <zeinersyad@gmail.com>
2023-07-29 14:59:23 +0700

    fix(Dropdown): position dropdown based on scroll overflow x axis

--- pages/transactions/index.vue
+++ pages/transactions/index.vue
@@ -28,7 +28,40 @@
         {{ isLoggedIn ? 'Sign Out' : 'Back to Login' }}
       </button>
     </div>
-    <div class="max-h-1/4 w-full gap-4 sm:flex justify-center my-8">
+      <general-dropdown id="dropdownCircles" class="mt-2">
+        <template #trigger="{activator}">
+          <button class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  type="button" @click="activator">
+            <span class="sr-only">Circles button</span>
+            Circles
+            <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                 viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m1 1 4 4 4-4"/>
+            </svg>
+          </button>
+        </template>
+        <template #content="{activator}">
+          <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200">
+            <li v-for="circle in circles">
+              <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                <div class="flex items-center h-5">
+                  <input v-model="filterDate" :id="circle?.id + `-radio`" :name="circle?.id + `-radio`"
+                         type="radio" :value="circle?.id"
+                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                         @change="onFilterDateChanges($event.target.value);">
+                </div>
+                <div class="ml-2 text-sm">
+                  <label :for="circle?.id + `-radio`" class="font-medium text-gray-900 dark:text-gray-300">
+                    <div>{{ capitalizeFirstLetter(circle?.name) }}</div>
+                  </label>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </template>
+      </general-dropdown>
+    <div class="max-h-1/4 w-full gap-4 sm:flex justify-center mb-8 mt-2">
       <expenses-structure-chart class="sm:w-1/2 md:w-1/4 lg:w-1/5 w-full" :label-time="filterDate"
                                 :transactions="transactions ?? null"/>
       <div class="sm:w-1/2 md:w-1/ h-full w-full flex-grow justify-between mt-4 sm:mt-0">
@@ -100,8 +133,7 @@
 
         <general-dropdown id="dropdownFilterCategoryButton">
           <template #trigger="{activator}">
-            <button data-dropdown-toggle="dropdownFilterCategory"
-                    class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            <button class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                     type="button" @click="activator">
               <span class="sr-only">Category</span>
               Category
@@ -113,7 +145,7 @@
             </button>
           </template>
           <template #content="{activator}">
-           <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownFilterCategoryButton">
+           <ul class="py-1 text-sm text-gray-700 dark:text-gray-200">
                 <li v-for="(category, index) of categories">
                   <div class="flex items-center px-2 py-1">
                     <input :id="`${category.id}-category-checkbox`" type="checkbox" :value="category.id"
