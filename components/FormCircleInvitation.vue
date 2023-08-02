f4d7e94e0647cfa01c7219b7c4648e35ef117559
Zein Ersyad <zeinersyad@gmail.com>
2023-08-02 21:55:24 +0700

    style(link invitation circle): make link to be clickable and link style

--- components/FormCircleInvitation.vue
+++ components/FormCircleInvitation.vue
@@ -2,7 +2,7 @@
   <div>
     <span class="font-semibold text-2xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-500 from-lime-600">{{ capitalizeFirstLetter(circle?.name) }}<small class="ml-2 font-light text-sm text-gray-500 dark:text-gray-400">Link Invitation</small></span>
     <blockquote class="p-4 my-4 border-l-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
-      <p class="text-sm italic font-medium leading-relaxed text-gray-900 dark:text-white">{{ copiedLink }}</p>
+      <a :href="copiedLink" target="_blank" class="text-sm italic font-medium leading-relaxed text-blue-900 underline dark:text-white">{{ copiedLink }}</a>
     </blockquote>
   </div>
 </template>
