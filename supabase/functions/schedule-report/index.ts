954aa086ea24b65cb732fda0d19e9e317fcda835
Zein Ersyad <zeinersyad@gmail.com>
2023-08-06 12:03:41 +0700

    fix: circle user length not actual member

--- supabase/functions/schedule-report/index.ts
+++ supabase/functions/schedule-report/index.ts
@@ -81,7 +81,7 @@ const handler = async (_request: Request): Promise<Response> => {
 
         const statusSent = []
 
-        for (let i = 0; i < 1; i++) {
+        for (let i = 0; i < circleUsers.length; i++) {
             const res = await fetch('https://qstash.upstash.io/v1/publish/https://xoixclvjgysqxybilnfk.supabase.co/functions/v1/send-email', {
                 method: 'POST',
                 headers: {
