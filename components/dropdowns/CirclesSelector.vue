87449daec6d275e9be437bad9edff474f57029ed
Zein Ersyad <zeinersyad@gmail.com>
2023-08-07 20:05:17 +0700

    fix: remove setting button on guest

--- components/dropdowns/CirclesSelector.vue
+++ components/dropdowns/CirclesSelector.vue
@@ -54,6 +54,7 @@
               </div>
             </div>
             <button
+                v-if="circleUser?.userId"
                 class="rounded-md text-center text-gray-500 bg-white border-none focus:ring-transparent hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 type="button" @click="onCircleChange(circleUser?.circle); modalSetting?.show()">
               <span class="sr-only">Setting</span>
