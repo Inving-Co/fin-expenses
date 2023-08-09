eb8852617e43cb0dafaef5db31a65a5f70ebc364
Zein Ersyad <zeinersyad@gmail.com>
2023-08-09 23:04:49 +0700

    fix(register): id user based on supabase user id

--- server/api/users/create.user.ts
+++ server/api/users/create.user.ts
@@ -1,7 +1,7 @@
 import {createUser} from "~/server/models/users";
 
 export default defineEventHandler(async (event) => {
-    const {email} = await readBody(event);
+    const {id, email} = await readBody(event);
 
-    return createUser(email)
+    return createUser(id, email)
 })
