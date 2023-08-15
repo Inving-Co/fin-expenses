ac091e559089b6fcb983ceffa673dc0096c57ed5
Zein Ersyad <zeinersyad@gmail.com>
2023-08-15 16:01:02 +0700

    style(color): changes blue color to primary color

--- components/dropdowns/FilterDates.vue
+++ components/dropdowns/FilterDates.vue
@@ -20,7 +20,7 @@
             <div class="flex items-center h-5">
               <input v-model="filterDate" :id="valueFilterDate + `-radio`" :name="valueFilterDate + `-radio`"
                      type="radio" :value="valueFilterDate"
-                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                     class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                      @change="onFilterDateChanges($event.target.value);">
             </div>
             <div class="ml-2 text-sm">
--- pages/index.vue
+++ pages/index.vue
@@ -18,14 +18,14 @@
             <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
               <input v-model="email" type="email" name="email" id="email"
-                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                      placeholder="name@company.com" :required="true" @keyup.enter="onSubmitLogin">
             </div>
             <div>
               <label for="password"
                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
               <input v-model="password" type="password" name="password" id="password" placeholder="••••••••"
-                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                      :required="true" @keyup.enter="onSubmitLogin">
             </div>
             <button type="button" :disabled="isLoadingSubmit"
