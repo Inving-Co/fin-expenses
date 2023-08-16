312ab6f10034631b8f5b14151025c5c917e859a2
Zein Ersyad <zeinersyad@gmail.com>
2023-08-16 21:31:48 +0700

    feat(category): update category

--- components/FormTransaction.vue
+++ components/FormTransaction.vue
@@ -31,8 +31,8 @@
       </div>
       <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category <span
           class="inline-flex cursor-pointer" @click="emit('edit-category'); isEditMode = !isEditMode">
-        <icons-edit v-if="!isEditMode" class="h-4 h-4"/>
-        <icons-close v-else class="h-4 h-4"/>
+        <icons-edit v-if="!isEditMode" class="h-4"/>
+        <icons-close v-else class="h-4"/>
       </span>
       </label>
       <div
@@ -40,22 +40,37 @@
           class="h-10 w-10 inline-flex align-bottom mx-2 p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-primary-500 peer-checked:border-primary-600 peer-checked:text-primary-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
           style="margin-top: 0"
           @click="emit('add-category')">
-        <icons-plus />
+        <icons-plus/>
       </div>
       <div v-else class="h-10 w-10 inline-flex mx-2 p-2" style="margin-top: 0"/>
-      <div v-for="category in categories.data" class="relative h-10 inline-flex items-center mb-4 mx-3">
-        <icons-close v-if="isEditMode" class="absolute -top-3 -left-2 w-5 h-5 rounded-md p-1 bg-red-500 text-white cursor-pointer" @click="onDeleteCategory(category.id)"/>
-        <icons-edit v-if="isEditMode" class="absolute -top-3 -right-2 w-5 h-5 rounded-md p-1 bg-primary-500 text-white cursor-pointer"/>
-        <input v-model="formTransaction.categoryId" :id="`radio-${category.id}`" type="radio" :value="category.id"
-               class="w-4 h-4 hidden peer text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-               :name="`radio-${category.id}`" required
-               @keyup.enter="onSave">
-        <label :for="`radio-${category.id}`"
-               class="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-primary-500 peer-checked:border-primary-600 peer-checked:text-primary-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-          <div class="text-lg font-semibold">
-            {{ capitalizeFirstLetter(category.name) }}
+      <div v-for="(category, index) of categories.data" class="relative h-10 inline-flex items-center mb-4 mx-3">
+        <div v-if="!category.edited">
+
+          <div v-if="isEditMode">
+            <icons-close class="absolute -top-3 -left-2 w-5 h-5 rounded-md p-1 bg-red-500 text-white cursor-pointer"
+                         @click="onDeleteCategory(category.id)"/>
+            <icons-edit class="absolute -top-3 -right-2 w-5 h-5 rounded-md p-1 bg-primary-500 text-white cursor-pointer"
+                        @click="category.edited = true"/>
           </div>
-        </label>
+          <input v-model="formTransaction.categoryId" :id="`radio-${category.id}`" type="radio" :value="category.id"
+                 class="w-4 h-4 hidden peer text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                 :name="`radio-${category.id}`" required
+                 @keyup.enter="onSave">
+          <label :for="`radio-${category.id}`"
+                 class="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-primary-500 peer-checked:border-primary-600 peer-checked:text-primary-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="text-lg font-semibold">
+              {{ capitalizeFirstLetter(category.name) }}
+            </div>
+          </label>
+        </div>
+        <div v-else class="relative align-bottom" style="margin-top: 0">
+          <icons-check class="absolute -top-3 -right-2 w-5 h-5 rounded-md p-1 bg-green-500 text-white cursor-pointer"
+                       @click=""/>
+          <input ref="inputRef" name="category-name" id="category-name" :value="category.name"
+                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-20 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                 type="text"
+                 placeholder="Example: Food" required v-on:keydown.enter="onUpdateCategory(index, category.id, $event.target.value)"/>
+        </div>
       </div>
 
       <button type="button"
@@ -184,6 +199,21 @@ async function onSave() {
   }
 }
 
+async function onUpdateCategory(index: number, categoryId: string, name: string) {
+  const {data, error, status} = await useFetch('/api/categories/update.category', {
+    method: 'POST',
+    body: JSON.stringify({
+      id: categoryId,
+      name,
+    }),
+  })
+
+  if (status.value === 'success') {
+    categories.value.data[index] = data.value as Category
+  } else {
+    toast.error(error.value?.statusMessage ?? '')
+  }
+}
 
 async function onDeleteCategory(categoryId: string) {
   const {error, status} = await useFetch('/api/categories/delete.category', {
