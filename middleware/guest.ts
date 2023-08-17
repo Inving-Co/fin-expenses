4b46645d3c90465aade4f3a1a3db6a2053f68e09
Zein Ersyad <zeinersyad@gmail.com>
2023-08-17 17:41:20 +0700

    feat(sign in): integration with google auth

--- middleware/guest.ts
+++ middleware/guest.ts
@@ -1,10 +1,37 @@
 import {useCookie, navigateTo} from "#app";
+import {useAuth} from "~/composables/auth";
+import {useLoading} from "~/composables/loading";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const refreshToken = useCookie('my-refresh-token')
-    const accessToken = useCookie('my-access-token')
+    const hash = from.hash.substring(1)
+    const parsed = hash.split('&')
 
-    if (refreshToken.value && accessToken.value) {
-        return navigateTo('/transactions')
+    if(hash) {
+        useLoading().value = true
+
+        let query: any = {}
+        for(let i = 0; i < parsed.length; i++) {
+            const kv = parsed[i].split('=')
+            query[kv[0]] = kv[1]
+        }
+        if(query) {
+            const maxAge = 100 * 365 * 24 * 60 * 60
+            document.cookie = `my-access-token=${query.access_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+            document.cookie = `my-refresh-token=${query.refresh_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+
+            await checkAuth()
+
+            document.cookie = `user-id=${useAuth().value?.userId}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+
+            useLoading().value = false
+            return navigateTo('/transactions')
+        }
+    } else {
+        const refreshToken = useCookie('my-refresh-token')
+        const accessToken = useCookie('my-access-token')
+
+        if (refreshToken.value && accessToken.value) {
+            // return navigateTo('/transactions')
+        }
     }
 })
