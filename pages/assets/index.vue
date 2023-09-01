f70d579a1045dc6b0094d9cda456ad95bc9dca5d
Zein Ersyad <zeinersyad@gmail.com>
2023-09-01 23:34:12 +0700

    feat: add dialog confirmation for delete data trx and assets

--- pages/assets/index.vue
+++ pages/assets/index.vue
@@ -6,6 +6,25 @@
                   @on-success="modalFormAsset?.hide(); selectedAsset = undefined; refreshAssets()"/>
     </template>
   </general-modal>
+
+  <general-modal id="modal-confirmation-delete" title="Confirmation" @on-mounted="modalConfDelete = $event">
+    <template #body>
+      <p class="text-gray-500">Are you sure you want to delete this data?</p>
+
+      <div class="flex mt-4">
+
+        <button 
+          type="button" 
+          class="text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+          @click="modalConfDelete?.hide()">No</button>
+        <button 
+          type="button" 
+          class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+          @click="onDelete(selectedAsset.id); modalConfDelete?.hide()">Yes</button>
+      </div>
+    </template>
+  </general-modal>
+
   <div class="pb-8 h-full">
     <div class="flex flex-wrap justify-between items-center">
       <div class="flex flex-col my-6">
@@ -66,7 +85,7 @@
                 <li>
                   <button type="button"
                           class="w-full text-left block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                          @click="onDelete(asset?.id)">Delete
+                          @click="selectedAsset = asset; modalConfDelete?.show()">Delete
                   </button>
                 </li>
               </ul>
@@ -117,6 +136,7 @@ const selectedAsset = ref<EditableAsset | undefined>()
 const searchKey = ref<string>('')
 
 let modalFormAsset: ElementEvent | null = null
+let modalConfDelete: ElementEvent | null = null
 let refreshInputAmount: any = null
 const selectedCircle = computed(() => $circleUsers.value.selected)
 
