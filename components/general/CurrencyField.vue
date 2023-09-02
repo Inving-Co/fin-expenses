7553f0d3decb5a7dc705e671528237bfbc6ec615
Zein Ersyad <zeinersyad@gmail.com>
2023-09-02 21:09:10 +0700

    fix: currency not available when update item

--- components/general/CurrencyField.vue
+++ components/general/CurrencyField.vue
@@ -1,5 +1,5 @@
 <template>
-    <input name="amount" id="amount" v-model="inputValue"
+    <input v-model="inputValue"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
         type="text" placeholder="Example: 20000" required />
 </template>
@@ -13,10 +13,6 @@ const emit = defineEmits(['on-change', 'update:modelValue'])
 const props = defineProps({
     modelValue: {
         type: Number,
-        required: true,
-    },
-    amount: {
-        type: String,
     },
 })
 
@@ -26,12 +22,13 @@ const amount = ref(0)
 const currency = ref('IDR')
 
 onMounted(() => {
-    initial(props.amount)
-    inputValue.value = ''
-})
-
-watch(() => props.amount, (val) => {
-    initial(val)
+    if (props.modelValue) {
+        inputValue.value = new Intl.NumberFormat('ID', {
+            style: 'currency',
+            currency: currency.value,
+            maximumFractionDigits: currency.value === 'IDR' ? 0 : 2
+        }).format(props.modelValue);
+    }
 })
 
 watch(() => $circleUsers.value.selected, (val: Circle | null) => {
@@ -41,14 +38,20 @@ watch(() => $circleUsers.value.selected, (val: Circle | null) => {
 })
 
 watch(() => props.modelValue, (val) => {
-    if(!val) {
+    if (!val) {
         inputValue.value = ''
+    } else {
+        inputValue.value = new Intl.NumberFormat('ID', {
+            style: 'currency',
+            currency: currency.value,
+            maximumFractionDigits: currency.value === 'IDR' ? 0 : 2
+        }).format(val);
     }
 })
 
 watchDebounced(() => amount.value, (value) => {
     emit('update:modelValue', value);
-}, {debounce: 1000})
+}, { debounce: 1000 })
 
 watch(() => inputValue.value, (val) => {
     if (val[val.length - 1] === ',' || val === '') {
@@ -61,7 +64,6 @@ watch(() => inputValue.value, (val) => {
 
         const maximumFractionDigits = curAmount.split('.') ? curAmount.split('.')[1]?.length ?? 0 : 0;
 
-
         inputValue.value = new Intl.NumberFormat('ID', {
             style: 'currency',
             currency: currency.value,
@@ -71,26 +73,5 @@ watch(() => inputValue.value, (val) => {
 
     emit('on-change', amount.value)
 })
-
-function initial(newAmount: string | undefined) {
-    const selectedCircle = $circleUsers.value.selected
-    if (selectedCircle) {
-        currency.value = selectedCircle?.currency
-    }
-
-    if (newAmount) {
-        inputValue.value = newAmount ?? ''
-        const curAmount = newAmount.replace(/[^0-9,]/g, '').replace(',', '.');
-        amount.value = +curAmount
-
-        const maximumFractionDigits = curAmount.split('.') ? curAmount.split('.')[1]?.length ?? 0 : 0;
-
-        inputValue.value = new Intl.NumberFormat('ID', {
-            style: 'currency',
-            currency: currency.value,
-            maximumFractionDigits: maximumFractionDigits
-        }).format(amount.value);
-    }
-}
 
 </script>
