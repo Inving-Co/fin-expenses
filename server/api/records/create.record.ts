1e237e24a63c338d7a7b516f32ceea5123d376aa
Zein Ersyad <zeinersyad@gmail.com>
2023-09-12 23:44:29 +0700

    fix(cookie): set selected circle max age to never expires

--- server/api/records/create.record.ts
+++ server/api/records/create.record.ts
@@ -4,13 +4,12 @@ import {eventFirstTrxCreated} from "~/utils/logsnag";
 export default defineEventHandler(async (event) => {
     const {date, description, amount, currency, categoryId, assetId} = await readBody(event);
 
-    console.log(date)
     const cookies = parseCookies(event)
     const userId = cookies['user-id']
     const circle = cookies['selected-circle'] ? JSON.parse(cookies['selected-circle']) : undefined
 
     let result = await createRecord(date, description, Number(amount), currency, categoryId, userId, circle?.id, assetId)
-    let transactions = await getRecords('', undefined, circle?.id, undefined)
+    let transactions = await getRecords('', undefined, circle?.id, undefined, undefined)
 
     if (transactions.length === 1 && result.user) {
         await eventFirstTrxCreated(result.user!.id, result.user!.email, result.id, result.description)
