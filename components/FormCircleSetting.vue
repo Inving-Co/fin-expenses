7c1605ef76b3e9d00c29dd1ac90390f7bd42242d
Zein Ersyad <zeinersyad@gmail.com>
2023-09-13 22:21:13 +0700

    fix: redirect signout

--- components/FormCircleSetting.vue
+++ components/FormCircleSetting.vue
@@ -28,7 +28,7 @@
       <client-only>
         <label class="my-2 mt-4 relative inline-flex items-center cursor-pointer"
           @click.prevent="toggleIsAmountVisible()">
-          <input type="checkbox" :checked="isAmountVisible" class="sr-only peer">
+          <input type="checkbox" :checked="!isAmountVisible" class="sr-only peer">
           <div
             class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-300 dark:peer-focus:ring-primary-800 rounded-full peer dark:bg-gray-400 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600">
           </div>
