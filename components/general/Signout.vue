df9663f88d76522bb5fbffd2e1435cb542d1862b
Zein Ersyad <zeinersyad@gmail.com>
2023-09-14 21:45:44 +0700

    fix: redirect of guest middleware

--- components/general/Signout.vue
+++ components/general/Signout.vue
@@ -1,7 +1,7 @@
 <template>
   <client-only>
     <button
-      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+      class="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
       type="button" @click="() => { onSignOut(); emit('on-signout') }">
       <span class="sr-only">{{ isLoggedIn ? 'Sign Out' : 'Back to Login' }}</span>
       {{ isLoggedIn ? 'Sign Out' : 'Back to Login' }}
