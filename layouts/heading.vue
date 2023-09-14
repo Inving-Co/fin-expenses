2d8636b3d92d7913a7832a41bdcce411c5f24827
Zein Ersyad <zeinersyad@gmail.com>
2023-09-14 18:11:10 +0700

    feat: add message link email is invalid

--- layouts/heading.vue
+++ layouts/heading.vue
@@ -98,7 +98,7 @@
                 </button>
               </div>
             </li>
-            <li>
+            <li v-if="isLoggedIn">
               <div class="block dark:hover:bg-gray-600">
                 <button
                   class="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
