7dd492d9273c8a76d73b1458fc79a60573dd4115
Zein Ersyad <zeinersyad@gmail.com>
2023-09-15 21:59:53 +0700

    fix: empty asset history on mobile device

--- components/AssetHistory.vue
+++ components/AssetHistory.vue
@@ -1,6 +1,6 @@
 import { Asset } from '../utils/types';
 <template>
-    <table v-if="!$isMobile()" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 sticky top-0 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
