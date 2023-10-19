53579958d2926c757b1bbd03479619aa7cd18870
Zein Ersyad <zeinersyad@gmail.com>
2023-10-19 23:06:20 +0700

    fix: currency field to be more simpler

--- components/general/CurrencyField.vue
+++ components/general/CurrencyField.vue
@@ -1,81 +1,91 @@
 <template>
+  <div class="flex items-center gap-4">
+    <span class="text-xl text-gray-500 font-bold dark:text-gray-300">{{currencySymbol}}</span>
     <input v-model="inputValue"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-        type="text" placeholder="Example: 20000" required :readonly="props.readOnly" />
+           @input="onInput"
+           :placeholder="`Example: ${currencySymbol} 20000`"
+           :readonly="readOnly"
+           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { Circle } from '~/utils/types';
-import { watchDebounced } from '@vueuse/shared';
-
-const emit = defineEmits(['on-change', 'update:modelValue'])
+import {ref, watch, defineProps, defineEmits} from 'vue';
 
 const props = defineProps({
-    modelValue: {
-        type: Number,
-    },
-    readOnly: {
-        type: Boolean,
-        default: false
-    }
-})
+  modelValue: Number,
+  readOnly: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const emit = defineEmits(['update:modelValue']);
 
 const $circleUsers = useCircleUsers()
-const inputValue = ref()
-const amount = ref(0)
-const currency = ref('IDR')
-
-onMounted(() => {
-    if (props.modelValue) {
-        inputValue.value = new Intl.NumberFormat('ID', {
-            style: 'currency',
-            currency: currency.value,
-            maximumFractionDigits: currency.value === 'IDR' ? 0 : 2
-        }).format(props.modelValue);
-    }
-})
+const inputValue = ref('');
+const currencySymbol = ref('');
 
 watch(() => $circleUsers.value.selected, (val: Circle | null) => {
-    if (val) {
-        currency.value = val?.currency
-    }
+  if (val) {
+    currencySymbol.value = getCurrencySymbol(val?.currency);
+  }
 })
 
 watch(() => props.modelValue, (val) => {
-    if (!val) {
-        inputValue.value = ''
-    } else {
-        inputValue.value = new Intl.NumberFormat('ID', {
-            style: 'currency',
-            currency: currency.value,
-            maximumFractionDigits: currency.value === 'IDR' ? 0 : 2
-        }).format(val);
+  inputValue.value = val ? formatValue(val) : '';
+});
+
+const onInput = () => {
+  const lastChar = inputValue.value.slice(-1);
+  if (lastChar === '.') {
+    // If the last character is a decimal delimiter, bypass the formatting
+    const sanitizedValue = inputValue.value.replace(/[^0-9.]/g, '');
+    const numericValue = parseFloat(sanitizedValue);
+    if (!isNaN(numericValue)) {
+      emit('update:modelValue', numericValue);
     }
-})
+    return;
+  }
 
-watchDebounced(() => amount.value, (value) => {
-    emit('update:modelValue', value);
-}, { debounce: 100 })
-
-watch(() => inputValue.value, (val) => {
-    if (val[val.length - 1] === ',' || val === '') {
-        return
+  const sanitizedValue = inputValue.value.replace(/[^0-9.]/g, '');
+  const numericValue = parseFloat(sanitizedValue);
+  if (!isNaN(numericValue)) {
+    emit('update:modelValue', numericValue);
+    const formattedValue = formatValue(numericValue);
+    // Update inputValue without triggering another input event
+    if (inputValue.value !== formattedValue) {
+      inputValue.value = formattedValue;
     }
+  }
+};
 
-    else if (val[val.length - 1] !== ',') {
-        const curAmount = val.replace(/[^0-9,]/g, '').replace(',', '.');
-        amount.value = +curAmount
+const isIDR = currencySymbol.value === 'IDR'
+const locale = isIDR ? 'id-ID' : 'en-US'
 
-        const maximumFractionDigits = curAmount.split('.') ? curAmount.split('.')[1]?.length ?? 0 : 0;
+const formatValue = (value: any) => {
+  const options = {
+    style: 'decimal',
+    maximumFractionDigits: isIDR ? 0 : 2,
+  };
+  return new Intl.NumberFormat(locale, options).format(value);
+};
 
-        inputValue.value = new Intl.NumberFormat('ID', {
-            style: 'currency',
-            currency: currency.value,
-            maximumFractionDigits: maximumFractionDigits
-        }).format(amount.value);
-    }
+const getCurrencySymbol = (currencyCode: any) => {
+  const parts = new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currencyCode
+  }).formatToParts(0);
+  const currencyPart = parts.find(part => part.type === 'currency');
+  return currencyPart ? currencyPart.value : '';
+};
 
-    emit('on-change', amount.value)
-})
 
 </script>
+
+<style scoped>
+.currency-input {
+  /* Your styles here */
+}
+</style>
