043ea2335b6f090435df8bf858b289174f16a88b
Zein Ersyad <zeinersyad@gmail.com>
2023-10-20 19:37:50 +0700

    feat: setup budget plan

--- server/models/bugets.ts
+++ server/models/bugets.ts
@@ -8,6 +8,35 @@ export async function createBudget(amount: number, userId: string | undefined, c
     })
 }
 
+export async function createOrUpdateBudget(amount: number, userId: string | undefined, circleId: string) {
+    const existingBudget = await prisma.circleBudgets.findFirst({
+        where: {
+            userId: userId,
+            circleId: circleId
+        }
+    });
+
+    if (existingBudget) {
+        return prisma.circleBudgets.update({
+            where: {
+                id: existingBudget.id
+            },
+            data: {
+                amount: amount
+            }
+        });
+    }
+    else {
+        return prisma.circleBudgets.create({
+            data: {
+                userId,
+                circleId,
+                amount
+            }
+        });
+    }
+}
+
 export function getBudgets(circleId: string | undefined) {
     return prisma.circleBudgets.findMany({
         where: {
